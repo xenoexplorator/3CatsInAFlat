eca6b07a1b86a0860071df44d667db80cb4ef73d
Boivin92 <boivin.92@gmail.com>
2017-04-22 13:40:23 -0400

    platforms now working!

--- Assets/Scripts/CollectableController.cs
+++ Assets/Scripts/CollectableController.cs
@@ -1,60 +1,120 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Timers;
-using UnityEngine;
-
-public enum CollectableType { Axe, Coal, Hat, Wheel, Wood };
-
-public class CollectableController : MonoBehaviour {
-
-	public CollectableType type;
-
-	private float examineTimer;
-
-	// Use this for initialization
-	void Start () {
-		this.examineTimer = 0.0f;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		if (this.examineTimer < 0) {
-			this.examineTimer = 0.0f;
-			transform.Find("Description").GetComponent<Renderer>().enabled = false;
-		} else if (this.examineTimer > 0) {
-			this.examineTimer -= Time.deltaTime;
-		}
-	}
-
-    void TakeItem(PlayerControllerScript player) {
-		if (this.type != CollectableType.Wood || player.HasItem(CollectableType.Axe)) {
-				player.AddItem(this.type);
-				Destroy(gameObject);
-            WarnManagers();
-		}
-		// TODO update sprite / object state
-	}
-	
-	void Examine() {
-		transform.Find("Description").GetComponent<Renderer>().enabled = true;
-		this.examineTimer = 3;
-	}
-
-    void WarnManagers()
-    {
-        switch (type){
-            case CollectableType.Axe:
-                
-                break;
-            case CollectableType.Coal:
-                break;
-            case CollectableType.Hat:
-                HouseManager.hatPicked = true;
-                break;
-            case CollectableType.Wheel:
-                break;
-            case CollectableType.Wood:
-                break;
-        }
-    }
-}
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using System.Timers;
+
+using UnityEngine;
+
+
+
+public enum CollectableType { Axe, Coal, Hat, Wheel, Wood };
+
+
+
+public class CollectableController : MonoBehaviour {
+
+
+
+	public CollectableType type;
+
+
+
+	private float examineTimer;
+
+
+
+	// Use this for initialization
+
+	void Start () {
+
+		this.examineTimer = 0.0f;
+
+    }
+
+	
+
+	// Update is called once per frame
+
+	void Update () {
+
+		if (this.examineTimer < 0) {
+
+			this.examineTimer = 0.0f;
+
+			transform.Find("Description").GetComponent<Renderer>().enabled = false;
+
+		} else if (this.examineTimer > 0) {
+
+			this.examineTimer -= Time.deltaTime;
+
+		}
+
+	}
+
+
+
+    void TakeItem(PlayerControllerScript player) {
+
+		if (this.type != CollectableType.Wood || player.HasItem(CollectableType.Axe)) {
+
+				player.AddItem(this.type);
+
+				Destroy(gameObject);
+
+            WarnManagers();
+
+		}
+
+		// TODO update sprite / object state
+
+	}
+
+	
+
+	void Examine() {
+
+		transform.Find("Description").GetComponent<Renderer>().enabled = true;
+
+		this.examineTimer = 3;
+
+	}
+
+
+
+    void WarnManagers()
+
+    {
+
+        switch (type){
+
+            case CollectableType.Axe:
+
+                
+
+                break;
+
+            case CollectableType.Coal:
+
+                break;
+
+            case CollectableType.Hat:
+
+                HouseManager.hatPicked = true;
+
+                break;
+
+            case CollectableType.Wheel:
+
+                break;
+
+            case CollectableType.Wood:
+
+                break;
+
+        }
+
+    }
+
+}
+
---
+++ Assets/StaircaseScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaircaseScript : MonoBehaviour {
+
+    public BoxCollider2D[] platforms;
+
+	// Use this for initialization
+	void Start () {
+        EnableCollision(true);
+	}
+
+    private void EnableCollision(bool state)
+    {
+        foreach (BoxCollider2D b in platforms)
+        {
+            b.enabled = state;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            EnableCollision(false);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            EnableCollision(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            EnableCollision(true);
+        }
+    }
+}
