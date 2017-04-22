b1ef35c27a87288b70b3e7b2bd7012c59bf55acd
Boivin92 <boivin.92@gmail.com>
2017-04-22 13:00:42 -0400

    Added main menu

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowControls()
+    {
+        //Show Game Controls, probably on a panel or something I guess. How am I supposed to know I'm not your boss, just a comment.
+    }
+}
