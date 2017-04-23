5b9ea26a1a4a0809698035cbeff64a16258351ab
Boivin92 <boivin.92@gmail.com>
2017-04-23 10:24:40 -0400

    pinescript

---
+++ Assets/Scripts/ObjectiveScripts/LivePineScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivePineScript : ObjectiveController
+{
+    Animator anim;
+
+    protected void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    new protected void Interact(PlayerControllerScript player)
+    {
+        if (true) //manager.pinetree == false
+        {
+            anim.SetTrigger("UnSnow");
+            //manager.pinetree = true;
+        }
+    }
+}
